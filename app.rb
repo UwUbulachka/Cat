2a0dd2526a3b6e6ea8f3b081f934e18d47dacfd0
UwUbulachka <meuw25@gmail.com>
2021-02-22 17:05:32 +0300

    Магазин коть

--- app.rb
+++ app.rb
@@ -11,6 +11,9 @@ class Client < ActiveRecord::Base # создание сущности
 end
 
 get '/' do
-  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb :index
 end
 
+get '/shop' do
+  "Hello World"
+end
